b49fc8dd84a09af91923d72d40e9ba9c81adc461
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2025-01-17 21:42:40 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 from app import create_app
 
+# Desactivar GPU para evitar conflictos en TensorFlow (si es relevante)
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 # Crear la aplicación Flask
 app = create_app()
 
 # Ejecutar el servidor
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Puerto especificado por Render o 5000 por defecto
+    port = int(os.getenv("PORT", 5000))  # Usar el puerto definido por Render o 5000 como predeterminado
     app.run(host="0.0.0.0", port=port, debug=True)
